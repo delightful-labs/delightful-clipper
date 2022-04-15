4404fb2d8cd3620f4da524612a411e8aef3f7e19
Matt Derocher <mjamesderocher@gmail.com>
2022-04-15 10:15:59 +0300

    More Service Worker Debugging

--- src/service-worker.js
+++ src/service-worker.js
@@ -18,46 +18,12 @@ self.addEventListener('activate', (event) => {
 	);
 });
 
-self.addEventListener('fetch', (event) => {
-	// @ts-expect-error
-	const { request } = event;
-  const url = new URL(request.url);
-
-	if (request.method !== 'GET' || request.headers.has('range') || url.pathname === '/add') return;
-
-	const cached = caches.match(request);
-
-	if (url.origin === location.origin && build.includes(url.pathname)) {
-		// always return build files from cache
-		// @ts-expect-error
-		event.respondWith(cached);
-	} else if (url.protocol === 'https:' || location.hostname === 'localhost') {
-		// hit the network for everything else...
-		const promise = fetch(request);
-
-		// ...and cache successful responses...
-		promise.then((response) => {
-			// cache successful responses
-			if (response.ok && response.type === 'basic') {
-				const clone = response.clone();
-				caches.open(name).then((cache) => {
-					cache.put(request, clone);
-				});
-			}
-		});
-
-		// ...but if it fails, fall back to cache if available
-		// @ts-expect-error
-		event.respondWith(promise.catch(() => cached || promise));
-	}
-});
-
 
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
   // If this is an incoming POST request for the
   // registered "action" URL, respond to it.
-  if (event.request.method === 'POST' && url.pathname === '/add') {
+  if (event.request.method === 'POST' && url.pathname === '/add/') {
     event.respondWith((async () => {
       const formData = await event.request.formData();
       const link = formData.get('link') || '';
