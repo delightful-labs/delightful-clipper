4ca0c86369ab63ce534870cedc873794ff44c981
Matt Derocher <mjamesderocher@gmail.com>
2022-04-15 07:16:23 +0300

    Added Service Worker and Manifest

--- src/lib/components/SettingsPanel.svelte
+++ src/lib/components/SettingsPanel.svelte
@@ -4,5 +4,7 @@
 
 <div>
   <h2>Settings</h2>
+  <!--TODO: line-height, font-family, margins/max-width, dark mode
+  Also add labels and display values-->
   <input type="range" bind:value={$userFontSize} min="10" max="48" />
 </div>
