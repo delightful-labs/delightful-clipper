505ee7f848c6add4adbff884e0b9467a4b3a1db1
Matt Derocher <mjamesderocher@gmail.com>
2022-04-16 08:17:01 +0300

    Expreimenting with Canvas and CanvasText

--- src/lib/components/SettingsPanel.svelte
+++ src/lib/components/SettingsPanel.svelte
@@ -6,20 +6,33 @@
   <summary>Settings</summary>
   <!--TODO: line-height, font-family, margins/max-width, dark mode
   Also turn into component-->
-  <div class="setting-container">
-    <label for={'userFontSize'}>Font Size: {$userFontSize}px</label>
-    <input id={'userFontSize'} type="range" bind:value={$userFontSize} min="10" max="48" />
-  </div>
+  <div class="settings-wrapper">
+    <div class="setting-container">
+      <label for={'userFontSize'}>Font Size: {$userFontSize}px</label>
+      <input id={'userFontSize'} type="range" bind:value={$userFontSize} min="10" max="48" />
+    </div>
 
-  <div class="setting-container">
-    <label for={'userContentWidth'}>Content Width: {$userContentWidth}%</label>
-    <input id={'userContentWidth'} type="range" bind:value={$userContentWidth} min="1" max="100" />
+    <div class="setting-container">
+      <label for={'userContentWidth'}>Content Width: {$userContentWidth}%</label>
+      <input id={'userContentWidth'} type="range" bind:value={$userContentWidth} min="1" max="100" />
+    </div>
   </div>
 </details>
 
 <style>
+  details {
+    position: relative;
+  }
+
   .setting-container {
     display: flex;
     align-items: center;
+  }
+
+  .settings-wrapper {
+    position: absolute;
+    top: 100%;
+    background-color: canvas;
+    padding: 1rem;
   }
 </style>
