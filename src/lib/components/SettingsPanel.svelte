e956d7a2f29360d67b1ca1c96ab44461ac480ec3
Matt Derocher <mjamesderocher@gmail.com>
2022-04-16 07:18:45 +0300

    Added styles for SettingsPanel

--- src/lib/components/SettingsPanel.svelte
+++ src/lib/components/SettingsPanel.svelte
@@ -2,9 +2,19 @@
   import { userFontSize } from '$lib/stores'
 </script>
 
-<div>
-  <h2>Settings</h2>
+<details>
+  <summary>Settings</summary>
   <!--TODO: line-height, font-family, margins/max-width, dark mode
   Also add labels and display values-->
-  <input type="range" bind:value={$userFontSize} min="10" max="48" />
-</div>
+  <div class="setting-container">
+    <label for={'userFontSize'}>Font Size: {$userFontSize}px</label>
+    <input id={'userFontSize'} type="range" bind:value={$userFontSize} min="10" max="48" />
+  </div>
+</details>
+
+<style>
+  .setting-container {
+    display: flex;
+    align-items: center;
+  }
+</style>
