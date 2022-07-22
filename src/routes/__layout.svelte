97d7898a03de05f45f5be149cc8d8ea7b20b3f9f
Matt Derocher <mjamesderocher@gmail.com>
2022-07-22 09:54:52 -0400

    Added back SettingsPanel

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,7 +3,7 @@
   import { browser } from '$app/env'
   import { userSettings, send, state } from '$lib/stores'
   //import ParseForm from '$lib/components/ParseForm.svelte'
-  //import SettingsPanel from '$lib/components/SettingsPanel.svelte'
+  import SettingsPanel from '$lib/components/SettingsPanel.svelte'
   import DetailsSummary from '$lib/components/DetailsSummary.svelte'
   import LoadingSpinner from '$lib/components/LoadingSpinner.svelte'
 
@@ -36,7 +36,7 @@
   <DetailsSummary title={'Add'}>
     <!--<ParseForm />-->
   </DetailsSummary>
-  <!--<SettingsPanel />-->
+  <SettingsPanel />
 </header>
 
 <main>
